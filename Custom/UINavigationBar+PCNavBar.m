7eec415dec6b69278ecd0ad8b9af37ac4d56453a
Qoo789 <qoo_789@163.com>
2017-10-13 15:50:32 +0800

    10.13 修复 UINavigationBar 的分类中导航栏渐变方法在 iOS 11 之后崩溃的 bug.

--- Custom/UINavigationBar+PCNavBar.m
+++ Custom/UINavigationBar+PCNavBar.m
@@ -36,11 +36,19 @@ static char overlayerkey;
          这里为什么Y要-20,H要+20呢?
          因为,overlayer要覆盖statusBar
          */
-        self.overlayer = [[UIView alloc] initWithFrame:CGRectMake(0, -20, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + 20)];
+        self.overlayer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + 20)]; // CGRectMake(0, -20, CGRectGetWidth(self.bounds), CGRectGetHeight(self.bounds) + 20)
         
         self.overlayer.userInteractionEnabled = NO;
-        self.overlayer.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight; // 这里有人说 不应该设置 UIViewAutoresizingFlexibleHeight;
-        [self insertSubview:self.overlayer atIndex:0];
+        self.overlayer.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+        // | UIViewAutoresizingFlexibleHeight;
+        // 这里UINavigationBar+Awesome的扩展里说 不应该设置 UIViewAutoresizingFlexibleHeight;
+        
+        // 替换_UIBarBackground
+        [self.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull view, NSUInteger i, BOOL * _Nonnull stop) {
+            if([view isKindOfClass: NSClassFromString(@"_UIBarBackground")]) {
+                [view insertSubview:self.overlayer atIndex:0];
+            }
+        }];
     }
     
     self.overlayer.backgroundColor = backgroundColor;
@@ -59,24 +67,42 @@ static char overlayerkey;
 // 设置导航栏上子控件的透明度
 - (void)pc_setElementsAlpha:(CGFloat)alpha {
     
-    [[self valueForKey:@"_leftViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
-        view.alpha = alpha;
-    }];
     
-    [[self valueForKey:@"_rightViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
-        view.alpha = alpha;
-    }];
-    
-    UIView *titleView = [self valueForKey:@"_titleView"];
-    titleView.alpha = alpha;
-    
-    // when viewController first load, the titleView maybe nil
-    [[self subviews] enumerateObjectsUsingBlock:^(UIView *obj, NSUInteger idx, BOOL *stop) {
-        if ([obj isKindOfClass: NSClassFromString(@"UINavigationItemView")]) {
-            obj.alpha = alpha;
-            *stop = YES;
-        }
-    }];
+    if (@available(iOS 11.0, *)) { // 系统版本为 iOS 11 以上
+        
+        // 设置 titleView 的透明度
+        [self.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull view, NSUInteger i, BOOL * _Nonnull stop) {
+            if([view isKindOfClass: NSClassFromString(@"_UINavigationBarContentView")]) {
+                [view.subviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                    if ([obj isKindOfClass:UILabel.class]) {
+                        obj.alpha = alpha;
+                    }
+                }];
+            }
+        }];
+        
+    } else {
+        
+        // 系统版本低于 iOS 11, navigationItem 属于 navigationBar 来管理, 这里需要改变透明度
+        [[self valueForKey:@"_leftViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+            view.alpha = alpha;
+        }];
+        
+        [[self valueForKey:@"_rightViews"] enumerateObjectsUsingBlock:^(UIView *view, NSUInteger i, BOOL *stop) {
+            view.alpha = alpha;
+        }];
+        
+        UIView *titleView = [self valueForKey:@"_titleView"];
+        titleView.alpha = alpha;
+        
+        // when viewController first load, the titleView maybe nil]
+        [[self subviews] enumerateObjectsUsingBlock:^(UIView *obj, NSUInteger idx, BOOL *stop) {
+            if ([obj isKindOfClass: NSClassFromString(@"UINavigationItemView")]) {
+                obj.alpha = alpha;
+                *stop = YES;
+            }
+        }];
+    }
 }
 
 // 重置导航栏
