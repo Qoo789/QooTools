74f3c76a3fff6393d90f0942a01b6346d46844ae
Qoo789 <qoo_789@163.com>
2017-10-25 14:29:46 +0800

    10.25 给 UIButton 增加了设置图片和文字相对位置及间距的方法, 返回加间距后 button 的实际尺寸.

--- Custom/UIButton+PCButton.h
+++ Custom/UIButton+PCButton.h
@@ -8,8 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, PCBtnImagePosition) {
+    PCBtnImagePosition_Left = 0,              //图片在左，文字在右
+    PCBtnImagePosition_Right = 1,             //图片在右，文字在左
+    PCBtnImagePosition_Top = 2,               //图片在上，文字在下
+    PCBtnImagePosition_Bottom = 3,            //图片在下，文字在上
+};
+
 @interface UIButton (PCButton)
 
 + (UIButton *)btnWithFrame:(CGRect)frame cornerRadius:(CGFloat)aCornerRadius borderColor:(UIColor*)color;
+
+// 需要 button 先设置好 frame 和 font 之后再调用该方法, 否则方法无效. 返回值是加入间距后 button 的实际尺寸
+- (CGSize)setImagePosition:(PCBtnImagePosition)postion andSpacing:(CGFloat)spacing;
 
 @end
--- Custom/UIButton+PCButton.m
+++ Custom/UIButton+PCButton.m
@@ -21,4 +21,56 @@
     return btn;
 }
 
+- (CGSize)setImagePosition:(PCBtnImagePosition)postion andSpacing:(CGFloat)spacing {
+    
+    CGFloat imageWith = self.imageView.image.size.width;
+    CGFloat imageHeight = self.imageView.image.size.height;
+    
+    CGFloat labelWidth = [self.titleLabel.text sizeWithAttributes:@{NSFontAttributeName: self.titleLabel.font}].width;
+    CGFloat labelHeight = [self.titleLabel.text sizeWithAttributes:@{NSFontAttributeName: self.titleLabel.font}].height;
+    
+    // image中心移动的 x 距离
+    CGFloat imageOffsetX = (imageWith + labelWidth) / 2 - imageWith / 2;
+    // image中心移动的 y 距离
+    CGFloat imageOffsetY = imageHeight / 2 + spacing / 2;
+    // label中心移动的 x 距离
+    CGFloat labelOffsetX = (imageWith + labelWidth / 2) - (imageWith + labelWidth) / 2;
+    // label中心移动的 y 距离
+    CGFloat labelOffsetY = labelHeight / 2 + spacing / 2;
+    
+    CGFloat realWidth = self.pc_width;
+    CGFloat realHeight = self.pc_height;
+    
+    switch (postion) {
+        case PCBtnImagePosition_Left:
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, -spacing/2, 0, spacing/2);
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, spacing/2, 0, -spacing/2);
+            realWidth = self.imageView.pc_width + self.titleLabel.pc_width + spacing;
+            break;
+            
+        case PCBtnImagePosition_Right:
+            self.imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth + spacing/2, 0, -(labelWidth + spacing/2));
+            self.titleEdgeInsets = UIEdgeInsetsMake(0, -(imageHeight + spacing/2), 0, imageHeight + spacing/2);
+            realWidth = self.imageView.pc_width + self.titleLabel.pc_width + spacing;
+            break;
+            
+        case PCBtnImagePosition_Top:
+            self.imageEdgeInsets = UIEdgeInsetsMake(-imageOffsetY, imageOffsetX, imageOffsetY, -imageOffsetX);
+            self.titleEdgeInsets = UIEdgeInsetsMake(labelOffsetY, -labelOffsetX, -labelOffsetY, labelOffsetX);
+            realHeight = self.imageView.pc_height + self.titleLabel.pc_height + spacing;
+            break;
+            
+        case PCBtnImagePosition_Bottom:
+            self.imageEdgeInsets = UIEdgeInsetsMake(imageOffsetY, imageOffsetX, -imageOffsetY, -imageOffsetX);
+            self.titleEdgeInsets = UIEdgeInsetsMake(-labelOffsetY, -labelOffsetX, labelOffsetY, labelOffsetX);
+            realHeight = self.imageView.pc_height + self.titleLabel.pc_height + spacing;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return CGSizeMake(realWidth, realHeight);
+}
+
 @end
