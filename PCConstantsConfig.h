8cce960c7111887b735dd60d9aec3a5cf67ae5ba
Qoo789 <qoo_789@163.com>
2017-11-13 17:19:41 +0800

    11.13 新增部分 iOS 11 系统及 iPhone X 尺寸适配判断

--- PCConstantsConfig.h
+++ PCConstantsConfig.h
@@ -19,8 +19,12 @@
 #define isBeforeiOS9 ([[[UIDevice currentDevice] systemVersion] floatValue] < 9.0)
 // iOS 9以上
 #define isLateriOS9  ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
+// iOS 10以下
+#define isBeforeiOS10  ([[[UIDevice currentDevice] systemVersion] floatValue] < 10.0)
 // iOS 10及以上
 #define isLateriOS10 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 10.0)
+// iOS 11及以上
+#define isLateriOS11 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 11.0)
 
 
 // 分辨率及屏幕尺寸相关
@@ -34,6 +38,7 @@
 #define isiPhone6 ([UIScreen mainScreen].bounds.size.height == 667)
 #define isiPhone6Plus ([UIScreen mainScreen].bounds.size.height == 736)
 #define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+#define isiPhoneX ([UIScreen mainScreen].bounds.size.height == 812)
 
 
 // 当前主窗口
@@ -56,15 +61,24 @@
 // 当前tabbar高度
 #define kCurrentTabHeight self.tabBarController.tabBar.frame.size.height
 
+// 状态栏高度(标准值), 适配 iPhone X
+#define kStatusBatHeight         (kScreenHeight == 812 ? 44 : 20)
 // 导航栏高度(标准值)
 #define kNaviHeight              44 // 不加状态栏高度
-#define kNaviHeightAddStatusBar  64 // 加上状态栏高度
-// tabbar高度(标准值)
-#define kTabbatHeight            49
+//#define kNaviHeightAddStatusBar  64
+#define kNaviHeightAddStatusBar  (kStatusBatHeight + kNaviHeight)
+// HomeIndicator高度, 适配 iPhone X
+#define kHomeIndicatorHeight     34
+// tabbar高度(标准值), 适配 iPhone X
+#define kTabbarHeight            (kScreenHeight == 812 ? (49 + kHomeIndicatorHeight) : 49)
 
 // 比例系数
 #define kWidthScale   kScreenWidth / 750
 #define kHeightScale  kScreenHeight / 1334
+// 812 是 iPhone X 的实际屏幕高度, 667.0表示是 iPhone 8 的高度
+//#define kHeightScale  kScreenHeight / 1334 // (kScreenHeight == 812 ? 667.0 / 1334 : kScreenHeight / 1334)
+// 第一个参数是当下的控制器适配iOS11 一下的，第二个参数表示scrollview或子类
+#define AdjustsScrollViewInsetNever(controller,view)  if (@available(iOS 11.0, *)) {view.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;} else if ([controller isKindOfClass:[UIViewController class]]) {controller.automaticallyAdjustsScrollViewInsets = false;}
 
 
 // 快捷方法
@@ -74,19 +88,22 @@
 // HEX颜色
 #define HEXCOLOR(hexValue) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16)) / 255.0 green:((float)((hexValue & 0xFF00) >> 8)) / 255.0 blue:((float)(hexValue & 0xFF)) / 255.0 alpha:1.0f]
 // 常用颜色
-#define PCTabBackColor         RGBCOLOR(255, 255, 255)
-#define PCTabTintColor         RGBCOLOR(0, 0, 0)
-#define PCNavBackColor         RGBCOLOR(255, 255, 255)
-#define PCNavTintColor         RGBCOLOR(0, 0, 0)
-#define PCMainColor            HEXCOLOR(0xff781f) // 主题色 #ff781f
-#define PCControllerBackColor  RGBCOLOR(255, 255, 255)
-#define PCCommonBtnTintColor   RGBCOLOR(255, 255, 255)
+#define PCTabBackColor          RGBCOLOR(255, 255, 255)
+#define PCTabTintColor          RGBCOLOR(0, 0, 0)
+#define PCNavBackColor          RGBCOLOR(255, 255, 255)
+#define PCNavTintColor          RGBCOLOR(0, 0, 0)
+#define PCMainColor             HEXCOLOR(0xff781f) // 主题色 #ff781f
+#define PCControllerBackColor   RGBCOLOR(255, 255, 255)
+#define PCCommonBtnTintColor    RGBCOLOR(255, 255, 255)
+#define PCSeparatorColor        HEXCOLOR(0xe7e7e7)
+#define PCBlackTextColor        HEXCOLOR(0x000000)
+#define PCDisabledColor         HEXCOLOR(0xb0b0b0)
+#define PCCommonCellTitleColor  HEXCOLOR(0x4a4a4a)
 // ---------------------------------------
-#define PCHintLabelColor       RGBCOLOR(117, 117, 117) // 提示label的字体颜色 #757575
-#define PCSeperatorColor       RGBCOLOR(236, 236, 236) // 首页模块分隔线颜色 #ececec
-#define PCTitleColor           RGBCOLOR(33, 33, 33) // 主要title字体颜色 #212121
-#define PCGrayBackColor        RGBCOLOR(247, 247, 248) // 浅灰色背景色 ##f7f7f8
-#define PCBackGroundColor      RGBCOLOR(243, 243, 245)
+#define PCHintLabelColor        RGBCOLOR(117, 117, 117) // 提示label的字体颜色 #757575
+#define PCTitleColor            RGBCOLOR(33, 33, 33) // 主要title字体颜色 #212121
+#define PCGrayBackColor         RGBCOLOR(247, 247, 248) // 浅灰色背景色 ##f7f7f8
+#define PCBackGroundColor       RGBCOLOR(243, 243, 245)
 
 // 设置系统字体字号
 #define SystemFont(size) [UIFont systemFontOfSize:size]
